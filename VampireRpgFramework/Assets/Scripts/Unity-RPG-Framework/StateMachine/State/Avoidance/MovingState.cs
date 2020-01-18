7cabc8b89b85a77da52bfa562e775d787589025e
CoffeeVampir3 <48565901+CoffeeVampir3@users.noreply.github.com>
2020-01-18 03:02:44 +0100

    w

---
+++ VampireRpgFramework/Assets/Scripts/Unity-RPG-Framework/StateMachine/State/Avoidance/MovingState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPGFramework {
+	[CreateAssetMenu]
+	public class MovingState : BaseState {
+		public override void OnEnterState(GameObject thisObject) {
+		}
+
+		public override void OnExitState(GameObject thisObject) {
+		}
+
+		public override void OnUpdate(GameObject thisObject) {
+			Transform t = thisObject.transform;
+			Rigidbody rb = thisObject.GetComponent<Rigidbody>();
+
+			rb.velocity += t.forward * .02f;
+
+			RaycastHit hit;
+			Debug.DrawRay(t.position, t.forward * 5f, Color.green, .05f);
+			if (Physics.Raycast(t.position, t.forward, out hit, 5f))
+			{
+				StateMachine sm = thisObject.GetComponent<StateMachine>();
+				sm.NextState();
+			}
+		}
+	}
+}
---
+++ VampireRpgFramework/Assets/Scripts/Unity-RPG-Framework/StateMachine/State/Avoidance/PickNewDirectionState.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPGFramework {
+	[CreateAssetMenu]
+	public class PickNewDirectionState : BaseState {
+
+		public Vector3 GetFreeDirection(GameObject thisObject) {
+			float angle = 0;
+			Transform t = thisObject.transform;
+			for (int i = 0; i < 16; i++)
+			{
+				float y = Mathf.Cos(angle);
+				angle += 2 * Mathf.PI / 16;
+
+				Vector3 dir = new Vector3(0, t.position.y + y, 0);
+				RaycastHit hit;
+				Debug.DrawLine(t.position, dir, Color.red, 10.0f);
+				if (Physics.Raycast(t.position, dir, out hit))
+				{
+					Debug.Log(dir);
+					return dir;
+				}
+			}
+			return Vector3.zero;
+		}
+
+		public override void OnEnterState(GameObject thisObject) {
+		
+		}
+
+		public override void OnExitState(GameObject thisObject) {
+
+		}
+
+		public override void OnUpdate(GameObject thisObject) {
+
+
+			Debug.Log("Changed directions!");
+			Rigidbody rb = thisObject.GetComponent<Rigidbody>();
+
+			rb.velocity = Vector3.zero;
+			Vector3 dir = GetFreeDirection(thisObject);
+
+			thisObject.transform.LookAt(new Vector3(thisObject.transform.rotation.eulerAngles.x + dir.y,
+				thisObject.transform.rotation.eulerAngles.y,
+				thisObject.transform.rotation.eulerAngles.z), thisObject.transform.up);
+
+
+			//StateMachine sm = thisObject.GetComponent<StateMachine>();
+			//sm.NextState();
+		}
+	}
+}
